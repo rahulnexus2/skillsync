ed802bff0d0df84bfff4664a024ea85d9e193508
rahulnexus2 <your-email@example.com>
2025-09-02 16:17:22 +0530

    fix: normalize line endings

---
+++ client/src/pages/Signup.jsx
@@ -0,0 +1,26 @@
+
+
+export function Signup(){
+return (<>
+  <div className="">
+    <h1>Signup by entering details</h1>
+    <label htmlFor="">Email</label>
+    <input type="email"  placeholder="enter your email" />
+    <label htmlFor="">Password</label>
+    <input type="password"  placeholder="enter your password" />
+    <button>Submit</button>
+  </div>
+
+
+</>)
+
+}
+
+
+
+
+
+
+
+
+
