2339b2715f4170ce79095c12ddfc24034c3213bc
rahulnexus2 <primenexus078@gmail.com>
2026-02-28 22:27:38 +0530

    changed backend url

--- server/server.js
+++ server/server.js
@@ -110,7 +110,7 @@ io.on("connection", (socket) => {
 
 
 app.use(cors({
-  origin: ["http://localhost:5173", "https://skillsync.vercel.app"],
+  origin: ["http://localhost:5173", "https://skillsync-steel.vercel.app"],
   credentials: true
 }));
 
